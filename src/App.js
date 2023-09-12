c08e408d679a1d208fd7ff412654312be4b7bdaf
Jerome Laflamme <jeromelaflammep@gmail.com>
2023-09-12 14:01:16 -0400

    cube animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import * as THREE from "three";
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75, window.innerWidth / window.innerHeight, 0.1, 1000
   );
+
+  const renderer = new THREE.WebGLRenderer();
+  
+  renderer.setSize( window.innerWidth , window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+
+  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  const material = new THREE.MeshBasicMaterial( { color: 0x61dafb } );
+  const cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+
+  camera.position.z = 5;
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+  }
+  
+
+
+  animate()
+  return <div className="App">
+
+  </div>;
 }
 
 export default App;
